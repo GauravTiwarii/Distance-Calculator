8cb26a29fe0e9d29ebf4d9d8f0e4a7b511e58f17
Love <Love@love.local>
2016-06-04 11:49:16 +0530

    Initial Commit

---
+++ Distance Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Distance Calculator
+//
+//  Created by Love on 6/4/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Distance Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Distance Calculator
+//
+//  Created by Love on 6/4/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
